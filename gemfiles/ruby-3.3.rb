a4a0bc7589080d5c51555ceea2e561493eb39221
Tony Hsu <tony.hsu@datadoghq.com>
2024-05-07 02:24:28 +0200

    Place check group last

--- gemfiles/ruby-3.3.rb
+++ gemfiles/ruby-3.3.rb
@@ -8,15 +8,15 @@ group :test do
   gem "minitest", "~> 5.22.3"
 end
 
+group :debug do
+  gem "debug"
+  gem "irb"
+end
+
 group :check do
   gem "rubocop", "~> 1.62.1", require: false
   gem "rubocop-minitest", "~> 0.33.0", require: false
   gem "rubocop-rake", "~> 0.6.0", require: false
   gem "standard", "~> 1.0", require: false
   gem "steep", "~> 1.6.0", require: false
-end
-
-group :debug do
-  gem "debug"
-  gem "irb"
 end

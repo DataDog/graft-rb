60bf31adf0559899e92d650945bc5bb9412e557a
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-07-16 14:30:47 +0200

    Use Minitest original reporter for VSCode

--- spec/spec_helper/minitest/reporter.rb
+++ spec/spec_helper/minitest/reporter.rb
@@ -4,4 +4,4 @@ require "minitest/reporters"
 
 require_relative "reporters/default_reporter"
 
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new unless %w[n no false 0].include?(ENV.fetch("MINITEST_REPORTER", "").downcase)

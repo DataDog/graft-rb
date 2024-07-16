997dd25ee892fe68f04b8f0f7496032a6fa20c6c
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-07-16 13:09:43 +0200

    Organise `spec_helper`s

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,3 @@
 # frozen_string_literal: true
 
-require "minitest/reporters"
-
-# Minitest::Reporters::DefaultReporter misreports `skip` location
-# as being in Minitest source instead of actual `skip` location
-# See: https://github.com/minitest-reporters/minitest-reporters/issues/356
-module FixSkipLocation
-  def location(exception)
-    exception.location
-  end
-end
-Minitest::Reporters::DefaultReporter.prepend FixSkipLocation
-
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require_relative "spec_helper/minitest/reporter"
